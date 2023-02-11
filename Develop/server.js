81ca322aecef4373813e6c04bd42fd683c42e9f0
jrwesch <jrwesch@gmail.com>
2023-02-11 14:38:05 -0500

    adding to server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const uuid = require('./helper/uuid');
 const dbData = require('./db/db.json');
 const PORT =  3001;
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 
 app.use(express.static('public'));
 
